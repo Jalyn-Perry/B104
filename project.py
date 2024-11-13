07f6c0965ec1eca594214bda74f91c51f11789da
Perry <JALYNMP@email.uscb.edu>
2024-11-13 13:06:50 -0500

    updated project file

--- project.py
+++ project.py
@@ -2,34 +2,28 @@
 # Course:        B104
 # Assignment:    ICE 12_2
 
+## Imports
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-
+## Creating dataframe and dataframe copy
 df1 = pd.read_csv("data.csv")
 df2 = df1.copy()
 
 
 X = df2
 
-print(df2.head())
+#print(df2.head())
 
-columns_to_drop = ["q1", "q2", "q3", "q4", "q5", "q6", "q7", "raceeth"]
-df3 = df2.drop(columns_to_drop, axis=1, inplace=True)
-#sample_df = df3.sample(n=2)
 
-# Plotting Area Plot
-plt.figure(figsize=(80, 60))
 
-df2.plot(kind='area', stacked=False)
-plt.title('Area Plot')
-plt.xlabel('q12')
-plt.ylabel('q17')
+countForQuestion12 = df2['q12'].value_counts()
+print(countForQuestion12) 
 
-plt.show()
-
+countForQuestion17 = df2['q17'].value_counts()
+print(countForQuestion17) 
 
 
 
